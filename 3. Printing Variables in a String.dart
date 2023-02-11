f5c2f43ffdca19705881edc036c999f44ae64dfa
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-11 14:59:00 +0600

    Module-1

---
+++ 3. Printing Variables in a String.dart
@@ -0,0 +1,11 @@
+// Printing Variables in a String
+
+void main()
+{
+  var age = 10;
+  var type = "cold";
+  var food = "$type Coffee";
+  
+  print("I am $age years old");
+  print("I love eating $food");
+}
