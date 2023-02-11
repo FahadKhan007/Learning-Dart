01944074acf524844fb4d8cb84065c7a634208f4
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-02-11 23:27:30 +0600

    Module-1

---
+++ 5. Variables.dart
@@ -0,0 +1,16 @@
+// Variables
+
+void main() {
+  
+//   declaring variable
+  var epicName;
+//   initializing variable
+  epicName = "Hello World";
+  
+//   declaring and initializing variable
+  String solidName = "Hello World";
+//   solidName = 20; // shows an error cause the datatype is not same
+  solidName = "Update";
+  print (solidName);
+  
+}
